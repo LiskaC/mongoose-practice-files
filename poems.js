f4e6a7e4019b5997972913a67ff8f55edbe47a4c
Liska Crofts <yscrofts@hotmail.com>
2020-05-16 14:24:01 +0200

    applied a Promise to return poems

--- poems.js
+++ poems.js
@@ -5,4 +5,47 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, "connection malfunction, connection malfunction"));
 db.once('open', function () {
 
+  var poemSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    date: Date
+  });
+
+  var Poem = mongoose.model('Poem', poemSchema);
+
+  var byzantium = new Poem({ name: "Sailing to Byzantium", author: "Yeats", date: 1928 })
+
+  const savePoem = (poem) => {
+    poem.save((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(`woot ${poem} saveeddd`);
+      }
+    })
+  }
+
+  savePoem(byzantium);
+
+  const listPoems = () => {
+    Poem.find({}, (err, poems) => {
+      poems.forEach((poem) => {
+        console.log('and the next poem is: ' + poem.name);
+      })
+    })
+  }
+
+  const promisePoems = () => {
+    Poem.find({})
+      .then((results) => {
+        console.log("here are all the poems you could possibly want: " + results)
+      })
+      .catch((err) => {
+        console.log("there was some error hmmm: " + err);
+      })
+  }
+
+
+  listPoems();
+  promisePoems();
 })
